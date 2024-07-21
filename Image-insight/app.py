c1546b2aed284084dc1d502ccd500031bb41a0fb
Ankit Shukla <163987630+ankit18006@users.noreply.github.com>
2024-07-21 20:11:13 +0530

    Add files via upload

---
+++ Image-insight/app.py
@@ -0,0 +1,32 @@
+import gradio as gr
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+import PIL.Image
+
+load_dotenv()
+
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+    
+
+def process_image(img_path, prompt):
+    print("Image Path:", img_path)
+    print("Prompt:", prompt)
+    
+    img = PIL.Image.open(img_path)
+    response = model.generate_content([prompt, img], stream=True)
+    response.resolve()
+    return response.text
+
+iface = gr.Interface(
+    fn=process_image,
+    inputs=[gr.Image(type="filepath"), gr.Textbox()],
+    outputs="text",
+    live=False,
+)
+
+
+iface.launch(share=True)
